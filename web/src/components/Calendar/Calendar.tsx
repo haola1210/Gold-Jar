1e1fab6a516c1e822533591294c4fb5e8a8c5db1
Le Anh Hao <hao.le-anh@internal.banvien.com>
2022-08-24 15:13:41 +0700

    add more functionalities and calendar legend

--- web/src/components/Calendar/Calendar.tsx
+++ web/src/components/Calendar/Calendar.tsx
@@ -5,14 +5,27 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 import { range } from '@utils/range';
+import { shalowCompareArray } from '@utils/shalowCompareArray';
 
 const weekDays = ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'];
 
+const legend = {
+  today: {
+    color: 'pink-100',
+    title: 'Ngày hôm nay'
+  },
+  selected: {
+    color: 'green-100',
+    title: 'Ngày đang chọn'
+  }
+};
 
 function Calendar() {
   
   const [day, setDay] = useState(() => dayjs());
   const today = dayjs();
+
+  const [selectedDate, selectDate] = useState<dayjs.Dayjs | null>(() => day);
 
   
   // 
@@ -41,8 +54,32 @@ function Calendar() {
     }
   };
 
+  const handleSelectDate = (date: number, month: number) => {
+    let dateObject: dayjs.Dayjs;
+    // Dec of last year
+    if(month === 0) {
+      dateObject = dayjs(`${thisYear-1}-${12}-${date}`);
+    } 
+    // Jan of next year
+    else if (month === 13) {
+      dateObject = dayjs(`${thisYear+1}-${1}-${date}`);
+    }
+    // this year
+    else {
+      dateObject = dayjs(`${thisYear}-${month}-${date}`);
+    }
+    
+    // selected before -> unselect
+    if(selectedDate && dateObject.isSame(selectedDate)) {
+      selectDate(null);
+    } else {
+      selectDate(dateObject);
+    }
+  };
+
+
   return (
-    <div className='w-full h-min px-0.5 pt-1'>
+    <div className='w-full h-min px-0.5 pt-1 pb-3 shadow-md rounded-xl'>
       {/* calendar header */}
       <div className="w-full flex h-8 justify-center items-center mb-1">
         <button 
@@ -52,7 +89,7 @@ function Calendar() {
           <ChevronDoubleLeftIcon className='w-4 h-4 m-1.5 text-blue-500' />
         </button>
         <p className="flex-grow text-center text-sm font-semibold mx-4 py-1 bg-orange-200 rounded-md">
-          { day.format('DD MMM YYYY') }
+          { selectedDate ? selectedDate.format('DD MMM YYYY') : day.format('MMM YYYY') }
         </p>
         <button 
           className='border-blue-400 border-2 rounded-full'
@@ -62,7 +99,7 @@ function Calendar() {
         </button>
       </div>
 
-      <div className="w-full">
+      <div className="w-full font-medium">
         {/* calendar week days */}
         <div className='w-full flex border-b-2 '>
           {
@@ -87,29 +124,44 @@ function Calendar() {
           
           {/* days of last month */}
           {
-            range(lastMonthDays).map(i =>
-              <div key={uuidv4()} className=" w-full text-center bg-slate-100 h-12 p-1 flex flex-col">
-                <div className=' h-4 leading-3 text-xs text-left '>
-                  {firstDate.subtract(firstDay - i, 'day').date()}
-                </div>
-                <div className='flex-grow' >
+            range(lastMonthDays).map(i => {
+              const thisDay = firstDate.subtract(firstDay - i, 'day');
+              return (
+                <div 
+                  key={uuidv4()} 
+                  className={
+                    `
+                      w-full text-center bg-slate-100 h-12 p-1 flex flex-col
+                      ${shalowCompareArray([selectedDate?.date(), selectedDate?.month(), selectedDate?.year()], [thisDay.date(), thisDay.month(), thisDay.year()]) ? 'bg-green-100' : ''}
+                    `
+                  }
+                  onClick={() => handleSelectDate(thisDay.date(), thisMonth)}
+                >
+                  <div className=' h-4 leading-3 text-xs text-left '>
+                    { thisDay.date() }
+                  </div>
+                  <div className='flex-grow' >
 
+                  </div>
                 </div>
-              </div>
-            )
+              );
+            })
           }
 
           {/* days of this month */}
           {
             range(daysInMonth).map(i => 
-              <div key={uuidv4()} className={
-                `
-                  w-full text-center h-12 p-1 flex flex-col
-                  ${today.date() === i+1 &&
-                    today.month() === thisMonth &&
-                    today.year() === thisYear ? 'bg-pink-100' : ''}
-                `
-              }>
+              <div 
+                key={uuidv4()} 
+                className={
+                  `
+                    w-full text-center h-12 p-1 flex flex-col
+                    ${shalowCompareArray([today.date(), today.month(), today.year()], [i+1, thisMonth, thisYear]) ? 'bg-pink-100' : ''}
+                    ${shalowCompareArray([selectedDate?.date(), selectedDate?.month(), selectedDate?.year()], [i+1, thisMonth, thisYear]) ? '!bg-green-100' : ''}
+                  `
+                }
+                onClick={() => handleSelectDate(i+1, thisMonth+1)}
+              >
                 <div className=' h-4 leading-3 text-xs text-left'>{i+1}</div>
                 <div className='flex-grow' >
                   
@@ -120,20 +172,46 @@ function Calendar() {
 
           {/* days of next month */}
           {
-            range(nextMonthDays).map(i => 
-              <div key={uuidv4()} className=" w-full text-center bg-slate-100 h-12 p-1 flex flex-col">
-                <div className=' h-4 leading-3 text-xs text-left'>
-                  {lastDate.add(i + 1, 'day').date()}
+            range(nextMonthDays).map(i => {
+              const thisDay = lastDate.add(i + 1, 'day');
+              return (
+                <div 
+                  key={uuidv4()} 
+                  className={
+                    `
+                      w-full text-center bg-slate-100 h-12 p-1 flex flex-col
+                      ${shalowCompareArray([selectedDate?.date(), selectedDate?.month(), selectedDate?.year()], [thisDay.date(), thisDay.month(), thisDay.year()]) ? 'bg-green-100' : ''}
+                    `
+                  }
+                  onClick={() => handleSelectDate(thisDay.date(), thisMonth+2)}
+                >
+                  <div className=' h-4 leading-3 text-xs text-left'>
+                    { thisDay.date() }
+                  </div>
+                  <div className='flex-grow' >
+                    
+                  </div>
                 </div>
-                <div className='flex-grow' >
-                  
-                </div>
-              </div>
-            )
+              );
+            })
           }
         </div>
       </div>
-      
+      <div className="mt-2 px-2 w-full flex justify-between text-sm text-gray-500 font-medium">
+        {Object.keys(legend).map(key => (
+          <div key={key} className='flex items-center'>
+            <span className={
+              `
+                block w-4 h-4
+                bg-${legend[key as keyof typeof legend].color}
+                rounded-full
+                outline outline-${legend[key as keyof typeof legend].color}
+              `
+            } />
+            <p className='px-2'>{legend[key as keyof typeof legend].title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ web/src/utils/shalowCompareArray.ts
@@ -0,0 +1,8 @@
+export const shalowCompareArray = <T>(baseArr: T[], arr: T[]) => {
+  for(const i in baseArr){
+    if(baseArr[i] !== arr[i]){
+      return false;
+    }
+  }
+  return true;
+};
