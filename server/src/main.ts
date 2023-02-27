c9f240d259532e2dc4739e5a5473ca0cc8c27b08
haola1210 <hao.le-anh@internal.banvien.com>
2023-02-27 16:47:49 +0700

    setup for https

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //
+  const httpsOptions = {
+    key: fs.readFileSync('./private-key.pem'),
+    cert: fs.readFileSync('./public-certificate.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   //#region
   app.enableCors({
