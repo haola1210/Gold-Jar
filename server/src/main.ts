405eaed1be181e9b79e156eb13d1a8959b221860
haola1210 <haola1210@gmail.com>
2023-02-27 21:00:57 +0700

    add cert for https

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,8 +7,8 @@ import * as fs from 'fs';
 async function bootstrap() {
   //
   const httpsOptions = {
-    key: fs.readFileSync('./private-key.pem'),
-    cert: fs.readFileSync('./public-certificate.pem'),
+    key: fs.readFileSync('./secret/private-key.pem'),
+    cert: fs.readFileSync('./secret/public-certificate.pem'),
   };
 
   const app = await NestFactory.create(AppModule, { httpsOptions });
@@ -33,6 +33,9 @@ async function bootstrap() {
     }),
   );
   //
-  await app.listen(process.env.PORT || 3333);
+  const port = process.env.PORT || 3333;
+  await app.listen(port, () => {
+    console.log(`Server at: https://localhost:${port}`);
+  });
 }
 bootstrap();
