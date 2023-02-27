34596fdfa73122e0b7d3f2164fd31e89ccf3f32f
huyvo1205 <huyvo1205@gitlab.banvien.com.vn>
2023-02-27 16:29:38 +0700

    done page login

--- web/src/consts/error-type.ts
+++ web/src/consts/error-type.ts
@@ -1,3 +1,5 @@
 export enum ERROR_TYPE {
   CONFLICT = 'Conflict',
+  NOT_FOUND = 'Not Found',
+  BAD_REQUEST = 'Bad Request',
 }
--- web/src/utils/convertError.ts
+++ web/src/utils/convertError.ts
@@ -3,13 +3,27 @@ import { ERROR_TYPE } from '@consts/error-type';
 export const converError = (error: any) => {
   const data = error?.response?.data;
   const errorType = data?.error;
-  const errorData = JSON.parse(data?.message ?? `{}`);
+  console.log({ errorType }, data?.message);
+
   let castedErrors = {};
   if (errorType === ERROR_TYPE.CONFLICT) {
+    const errorData = JSON.parse(data?.message ?? `{}`);
     castedErrors = Object.keys(errorData).reduce((e, key) => {
       e[key] = `${key} đã tồn tại`;
       return e;
     }, {} as Record<string, string>);
+  }
+
+  if (errorType === ERROR_TYPE.NOT_FOUND) {
+    if (data?.message === 'User does not exist') {
+      castedErrors = { username: 'Không tồn tại tài khoản!' };
+    }
+  }
+
+  if (errorType === ERROR_TYPE.BAD_REQUEST) {
+    if (data?.message === 'Password is not correct') {
+      castedErrors = { password: 'Sai mật khẩu!' };
+    }
   }
 
   return castedErrors;
