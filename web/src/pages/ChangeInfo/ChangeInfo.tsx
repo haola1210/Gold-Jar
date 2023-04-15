3ecce84bdf955f02ec57733df6647aac8e726d8f
huyvo1205 <huyvo1205@gitlab.banvien.com.vn>
2023-04-15 14:42:54 +0700

    done module change name and change password

--- web/src/pages/ChangeInfo/ChangeInfo.tsx
+++ web/src/pages/ChangeInfo/ChangeInfo.tsx
@@ -3,33 +3,38 @@ import H1 from '@components/H1';
 import InputWithError from '@components/InputWithError';
 import Layout from '@components/Layout';
 import { useAuthContext } from '@contexts/AuthContext';
+import { updateUserName } from '@services/user.service';
 import { useFormik } from 'formik';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import * as yup from 'yup';
 
 const schema = yup.object({
   name: yup.string().required('Chưa nhập tên nè !!!').min(3, 'Tên phải dài hơn 2 ký tự'),
-  username: yup
-    .string()
-    .required('Chưa nhập tên đăng nhập nè!!!')
-    .min(6, 'Tên đăng nhập phải dài hơn 5 ký tự')
-    .max(25, 'Tên đăng nhập phải ngắn hơn 26 ký tự'),
-  email: yup.string().required('Chưa nhập email nè!!!').email('Chưa đúng định dạng Email nhaa!!!'),
 });
 
 const ChangeInfo = () => {
   const navigate = useNavigate();
-  const { user } = useAuthContext();
+  const { user, login } = useAuthContext();
   const form = useFormik({
     validateOnChange: false,
     initialValues: {
       name: user?.name,
-      email: user?.email,
     },
     validationSchema: schema,
-    onSubmit(value) {
-      console.log(value);
+    async onSubmit(value) {
+      const payload = {
+        name: value.name,
+      };
+      const user = await updateUserName(payload);
+      if (user) {
+        toast('Cập nhật tên thành công');
+        login?.(user);
+        navigate('/user-detail');
+      } else {
+        toast('Có gì đó đang sai');
+      }
     },
   });
 
@@ -41,7 +46,7 @@ const ChangeInfo = () => {
   return (
     <Layout>
       <div style={{ padding: '24px 5px' }}>
-        <H1 className='font-medium ml-2 mb-10 text-sky-400'>Thay đổi thông tin tài khoản</H1>
+        <H1 className='font-medium ml-2 mb-10 text-sky-400'>Thay đổi tên</H1>
         <div className='mx-2 border-dashed rounded-lg border-2 p-5 text-lg'>
           <div className='mb-4'>
             <div className='font-bold'>Tên:</div>
@@ -52,15 +57,7 @@ const ChangeInfo = () => {
               errorMessage={form.errors.name}
             />
           </div>
-          <div className='mb-4'>
-            <div className='font-bold'>Email:</div>
-            <InputWithError
-              name='email'
-              value={form.values.email}
-              onChange={handleChangeValue}
-              errorMessage={form.errors.email}
-            />
-          </div>
+
           <div className='flex justify-end mt-4 gap-2'>
             <Button
               className='block py-2 px-4 bg-green-600 font-bold text-white rounded-lg'
--- web/src/services/user.service.ts
+++ web/src/services/user.service.ts
@@ -1,7 +1,19 @@
 import { type User } from '@contexts/AuthContext';
 import { privateInstance } from './axios';
 
+const usersPath = `/users`;
+
 export const me = async () => {
-  const { data } = await privateInstance.get('/users/me');
+  const { data } = await privateInstance.get(`${usersPath}/me`);
+  return data as User;
+};
+
+export const updateUserName = async (payload: { name?: string }) => {
+  const { data } = await privateInstance.post(`${usersPath}/change-name`, payload);
+  return data as User;
+};
+
+export const updateUserPassword = async (payload: { oldPass: string; newPass: string }) => {
+  const { data } = await privateInstance.post(`${usersPath}/change-pass`, payload);
   return data as User;
 };
